b53690727e12747238ff9d7c16f3c9c49bab5371
itIsMuse <oyeniranshock@gmail.com>
2025-02-28 00:35:49 +0100

    Add login endpoint for user authentication

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hi i am listening ');
 });
 
+app.post('/login', (req, res) => {
+  res.send('Login successful');
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
